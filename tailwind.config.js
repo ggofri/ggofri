04808108a899b3b10b99aa62a1bd381001a943f6
Julian Ramirez <ignaciojulianramirez@gmail.com>
2025-07-28 19:40:43 -0300

    chore: adds tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,md,mdx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+}
+
