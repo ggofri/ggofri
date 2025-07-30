aea27314580e27a180d95bb0b9ceec54d3e43ee1
Julian Ramirez <ignaciojulianramirez@gmail.com>
2025-07-30 09:51:40 -0300

    chore: update site to vercel default deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://ggofri.vercel.app',
 	integrations: [mdx(), sitemap(), tailwind()],
 });
